0bfaa8202982c5ef999f0abc5451b1317f58d56d
Tomas Valdes <tvaldes@wisc.edu>
2024-04-30 16:14:03 -0500

    initial commit

---
+++ src/main/java/engine/snakesss/SnakesssApplication.java
@@ -0,0 +1,13 @@
+package engine.snakesss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakesssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/engine/snakesss/SnakesssApplicationTests.java
@@ -0,0 +1,13 @@
+package engine.snakesss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
